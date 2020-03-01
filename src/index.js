8fcbc3230f0bf9fb4558153df930ce8fd051a324
CaduLima <carlos.lima.exe@gmail.com>
2020-03-01 16:31:29 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -11,6 +11,13 @@ const server = require('http').Server(app);
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 
 const port = process.env.PORT || 8080;
 
