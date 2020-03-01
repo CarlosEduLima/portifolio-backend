433105dcb6b56cda721234840da215267e07eeac
CaduLima <carlos.lima.exe@gmail.com>
2020-03-01 17:33:23 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -8,16 +8,9 @@ const routes = require('./routes');
 const app = express();
 const server = require('http').Server(app);
 
-app.use(cors());
+app.use(cors({origin:'https://portifoliocarlos.herokuapp.com/'}));
 app.use(express.json());
 app.use(routes);
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
 
 const port = process.env.PORT || 8080;
 
